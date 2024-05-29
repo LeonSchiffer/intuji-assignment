dede221b81ac8f20845368b6a88b37637d0153f5
Bishal Gurung <leon.schiffer360@gmail.com>
2024-05-29 23:36:54 +0545

    feat: added spinner to button while event is being created

--- resources/js/Components/Events/EventCreate.vue
+++ resources/js/Components/Events/EventCreate.vue
@@ -1,43 +1,46 @@
 <template>
-<div class="pa-4 text-center">
-    <v-dialog v-model="dialog" max-width="600">
-        <template v-slot:activator="{ props: activatorProps }">
-            <v-btn class="text-none font-weight-regular" prepend-icon="mdi-calendar-plus" text="Create Event"
-                variant="tonal" v-bind="activatorProps"></v-btn>
-        </template>
-        <form @submit="createEvent">
-            <v-card prepend-icon="mdi-account" title="User Profile">
-                <v-card-text>
-                    <v-row dense>
-                        <v-col cols="12" sm="12">
-                            <v-text-field label="Title*" v-model="event.title" required></v-text-field>
-                        </v-col>
+    <div class="pa-4 text-center">
+        <v-dialog v-model="dialog" max-width="600">
+            <template v-slot:activator="{ props: activatorProps }">
+                <v-btn class="text-none font-weight-regular" prepend-icon="mdi-calendar-plus" text="Create Event"
+                    variant="tonal" v-bind="activatorProps"></v-btn>
+            </template>
+            <form @submit="createEvent">
+                <v-card prepend-icon="mdi-account" title="User Profile">
+                    <v-card-text>
+                        <v-row dense>
+                            <v-col cols="12" sm="12">
+                                <v-text-field label="Title*" v-model="event.title" required></v-text-field>
+                            </v-col>
 
-                        <v-col cols="12" sm="6">
-                            <v-text-field label="Start Date Time" v-model="event.start_time"
-                                type="datetime-local" required></v-text-field>
-                        </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-text-field label="Start Date Time" v-model="event.start_time" type="datetime-local"
+                                    required></v-text-field>
+                            </v-col>
 
-                        <v-col cols="12" sm="6">
-                            <v-text-field label="End Date Time" v-model="event.end_time"
-                                type="datetime-local" required></v-text-field>
-                        </v-col>
-                    </v-row>
-                </v-card-text>
+                            <v-col cols="12" sm="6">
+                                <v-text-field label="End Date Time" v-model="event.end_time" type="datetime-local"
+                                    required></v-text-field>
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
 
-                <v-divider></v-divider>
+                    <v-divider></v-divider>
 
-                <v-card-actions>
-                    <v-spacer></v-spacer>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
 
-                    <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
+                        <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
 
-                    <v-btn color="primary" text="Save" type="submit" variant="tonal"></v-btn>
-                </v-card-actions>
-            </v-card>
-        </form>
-    </v-dialog>
-</div>
+                        <v-btn color="primary" type="submit" variant="elevated">
+                            <v-label v-if="isFormEnabled">Save</v-label>
+                            <v-progress-circular v-else color="white" indeterminate></v-progress-circular>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </form>
+        </v-dialog>
+    </div>
 </template>
 <script>
 import axios from 'axios';
@@ -50,17 +53,22 @@ export default {
             title: "",
             start_time: "",
             end_time: ""
-        }
+        },
+        isFormEnabled: true
     }),
     methods: {
         createEvent(e) {
             e.preventDefault()
+            if (!this.isFormEnabled)
+                return
+            this.isFormEnabled = false
             console.log(this.event)
             axios
                 .post("/api/events", this.event)
                 .then(response => {
                     this.dialog = false
                     this.$emit('event-created', response.data)
+                    this.clearEventModel()
                     toast.success("Event successfully created")
                 })
                 .catch(err => {
@@ -73,6 +81,16 @@ export default {
                     }
                     toast.error(err.response.data.message)
                 })
+                .finally(() => {
+                    this.isFormEnabled = true
+                })
+        },
+        clearEventModel() {
+            this.event = {
+                title: "",
+                start_time: "",
+                end_time: ""
+            }
         }
     }
 }
